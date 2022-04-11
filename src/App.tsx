7573c1c953e7ca34630da7d5e296089de0921ec4
OswalDev <oswaldojaviersoto@gmail.com>
2022-04-11 16:44:07 -0400

    header UI and sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -19,55 +19,51 @@ import {
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import AllActivities from './pages/AllActivities/AllActivities';
-import AddActivities from './pages/AddActivities/AddActivities';
-import {homeOutline, accessibilityOutline, addOutline} from 'ionicons/icons';
+import AddActivity from './pages/AddActivity/AddActivity';
+import {homeOutline, accessibilityOutline, addOutline, bodyOutline, newspaperOutline} from 'ionicons/icons';
+import ActivitiesContextProvider from './data/ActivitiesContextProvider';
 
 /* Theme variables */
 import './theme/variables.css';
 
+
 setupIonicReact();
 
 const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-        <Route path="/all-activities" component={AllActivities} exact/>
-        <Route path="/add-activities" component={AddActivities} exact/>
-        <IonMenu side="start" contentId="Taskapp">
-          <IonHeader>
-            <IonToolbar color="primary">
-              <IonTitle>Task app</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          <IonContent>
-            <IonList>
-            <IonMenuToggle>
-                <IonItem routerLink='/all-activities' routerDirection='none' lines='none'>
-                  <IonIcon color='medium' slot='start' icon={homeOutline}/>
-                  <IonLabel>Home</IonLabel>
-                </IonItem>
-              </IonMenuToggle>
-              <IonMenuToggle>
-                <IonItem routerLink='/all-activities' routerDirection='none' lines='none'>
-                  <IonIcon color='medium' slot='start' icon={accessibilityOutline}/>
-                  <IonLabel>All activities</IonLabel>
-                </IonItem>
-              </IonMenuToggle>
-              <IonMenuToggle>
-                <IonItem routerLink='/add-activities' routerDirection='none' lines='none'>
-                  <IonIcon color='medium' slot='start' icon={addOutline}/>
-                  <IonLabel>Add activities</IonLabel>
-                </IonItem>
-              </IonMenuToggle>
-            </IonList>
-          </IonContent>
-        </IonMenu>
-    <IonRouterOutlet id='Taskapp'>
-      <Route path='/all-activities' component={AllActivities} exact />
-      <Route path='/add-activities' component={AddActivities} exact />
-      <Redirect to='/all-activities' />
-    </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
+<IonApp>
+        <IonReactRouter>
+            <IonMenu contentId='TaskApp'>
+                <IonHeader>
+                    <IonToolbar>
+                        <IonTitle>Task App</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent>
+                    <IonList>
+                        <IonMenuToggle>
+                            <IonItem routerLink="/all-activities" routerDirection="none" lines="none">
+                                <IonIcon color="medium" slot="start" icon={bodyOutline}/>
+                                <IonLabel>All activities</IonLabel>
+                            </IonItem>
+                        </IonMenuToggle>
+                        <IonMenuToggle>
+                            <IonItem routerLink="/add-activity" routerDirection="none" lines="none">
+                                <IonIcon color="medium" slot="start" icon={newspaperOutline}/>
+                                <IonLabel>Add activity</IonLabel>
+                            </IonItem>
+                        </IonMenuToggle>
+                    </IonList>
+                </IonContent>
+            </IonMenu>
+            <ActivitiesContextProvider>
+                <IonRouterOutlet id="TaskApp">
+                    <Route path='/all-activities' component={AllActivities} exact />
+                    <Route path='/add-activity' component={AddActivity} exact />
+                    <Redirect to='/all-activities' />
+                </IonRouterOutlet>
+            </ActivitiesContextProvider>
+        </IonReactRouter>
+    </IonApp>
 );
 
 export default App;
--- src/pages/AllActivities/AllActivities.tsx
+++ src/pages/AllActivities/AllActivities.tsx
@@ -1,31 +1,75 @@
-import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonMenuButton, IonPage, IonRadio, IonRow, IonTitle, IonToolbar } from "@ionic/react";
-import React from "react";
+import React, { useContext, useState } from 'react';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonButtons, IonMenuButton, IonRow, IonCol, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonItem, IonButton, IonModal, IonIcon } from '@ionic/react';
+import ActivitiesContext, { Activity } from '../../data/ActivitiesContext';
+import classes from './AllActivities.module.css';
+import CompleteModalActivity from '../../components/CompleteActivityModal';
+import { checkmarkOutline } from 'ionicons/icons';
 
 const AllActivities: React.FC = () => {
 
-    return(
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonButton slot="start">
-                        <IonMenuButton/>
-                    </IonButton>
-                    <IonTitle>All activities</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent>
-                <IonGrid>
-                    <IonRow>
-                        <IonCol>
-                            <h1>Here are your activities</h1>
-                        </IonCol>
-                    </IonRow>
-                </IonGrid>
-            </IonContent>
+    const [activityToComplete, setActivityToComplete] = useState<Activity>();
 
-        </IonPage>    
+    const activitiesCtxt = useContext(ActivitiesContext);
+
+    const openCompleteModal = (activity: Activity) => {
+        setActivityToComplete(activity);
+    };
+
+    const closeModal = () => {
+        setActivityToComplete(undefined);
+    };
+
+    return (
+        <React.Fragment>
+            <IonModal isOpen={!!activityToComplete}>
+                <CompleteModalActivity activity={activityToComplete as Activity} dismissModal={closeModal}/>
+            </IonModal>
+
+            <IonPage>
+                <IonHeader>
+                    <IonToolbar>
+                        <IonButtons slot="start">
+                            <IonMenuButton />
+                        </IonButtons>
+                        <IonTitle>All activities</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent>
+                    <IonGrid>
+                        { activitiesCtxt.activities.map(activity => (
+                        <IonRow key={activity.id}>
+                            <IonCol className="ion-text-center">
+                                <IonCard>
+                                    <img src={activity.imageUrl} alt="Activity"/>
+                                    <IonCardHeader>
+                                        <IonCardSubtitle>{activity.hour}</IonCardSubtitle>
+                                        <IonCardTitle>{activity.title}</IonCardTitle>
+                                    </IonCardHeader>
+                                    <IonCardContent>
+                                        <p>{activity.description}</p>
+                                        <IonItem lines="none">
+                                            { !activity.isCompleted ?
+                                            <IonButton
+                                                className={classes.CenterElement}
+                                                fill="clear"
+                                                onClick={() => openCompleteModal(activity)}>
+                                                Complete Activity
+                                            </IonButton>
+                                            :
+                                            <IonIcon color="success" className={classes.CenterElement} icon={checkmarkOutline} />
+                                            }
+                                        </IonItem>
+                                    </IonCardContent>
+                                </IonCard>
+                            </IonCol>
+                        </IonRow>
+                        ))
+                        }
+                    </IonGrid>
+                </IonContent>
+            </IonPage>
+        </React.Fragment>
     );
-
 };
 
 export default AllActivities;
